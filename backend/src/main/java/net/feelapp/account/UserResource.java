3b48cc0f80c683995f0754611cc75dcd00c96897
kula <kula@live.com>
2021-08-04 18:03:00 +0800

    feat: #79 write reactive service

---
+++ backend/src/main/java/net/feelapp/account/UserResource.java
@@ -0,0 +1,20 @@
+package net.feelapp.ebiz.account;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import io.smallrye.mutiny.Uni;
+import javax.inject.Inject;
+
+@Path("/api/login")
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource{
+
+    @GET
+    @Path("account")
+    public Uni<User> getLoginAccount(){
+        return User.getUser();
+    }
+}
