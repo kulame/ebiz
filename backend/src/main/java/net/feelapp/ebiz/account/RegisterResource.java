bcd3b967000480773c3ad8f609d3a5493c205607
kula <kula@live.com>
2021-08-05 18:02:46 +0800

    feat: #79 write create user interface

--- backend/src/main/java/net/feelapp/ebiz/account/RegisterResource.java
+++ backend/src/main/java/net/feelapp/ebiz/account/RegisterResource.java
@@ -6,17 +6,41 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.inject.Inject;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
+import java.time.LocalDateTime;
+import java.util.function.Function;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Path("api/register")
 @Produces(MediaType.APPLICATION_JSON)
 public class RegisterResource {
 
+    @Inject
+    io.vertx.mutiny.pgclient.PgPool client;
+
+    private static String create_user_sql = """
+                insert into ebiz_users 
+                    (email,password_crypt,date_joined) 
+                    values ($1,$2,$3) on conflict (email) do nothing 
+            """;
+
     @POST
     public Uni<RegisterResp> add(RegisterReq req){
         System.out.println(req.getEmail());
-        RegisterResp resp = new RegisterResp();
-        resp.setStatus("ok");
-        return  Uni.createFrom().item(resp);
+        var encoder = new BCryptPasswordEncoder();
+        var password_crypt = encoder.encode(req.getPassword());
+        return client.preparedQuery(create_user_sql)
+                .execute(Tuple.of(req.getEmail(),password_crypt, LocalDateTime.now()))
+                .map(pgRowSet -> {
+                    RegisterResp resp = new RegisterResp();
+                    resp.setStatus("ok");
+                    return resp;
+                });
     }
 
 }
